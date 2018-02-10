71b6d3f5aede2d55695d43660d3c59ae653d307e
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-10 14:11:17 -0400

    Showing all images in gallery.

---
+++ src/Components/Images/Images.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {CloudinaryContext, Image} from 'cloudinary-react'
+import '../../App.css'
+
+const images = (props) => {
+  return (
+    <CloudinaryContext className="row">
+      {
+        props.images.map((image, index) => {
+          return (
+            <div className="col-sm-3" key={index}>
+              <Image
+                className="image"
+                publicId={image.public_id}
+                cloudName="dl3e0kbis"/>
+            </div>
+          )
+        })
+      }
+    </CloudinaryContext>
+  )
+}
+
+export default images
--- src/Containers/Gallery/Gallery.js
+++ src/Containers/Gallery/Gallery.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import Images from '../../Components/Images/Images'
 
 class Videos extends Component {
+  state = {
+    images: []
+  }
+
+  getImages () {
+    axios.get('	http://res.cloudinary.com/dl3e0kbis/image/list/imagefy.json')
+    .then(res => {
+      this.setState({images: res.data.resources});
+    });
+  }
+
+  componentDidMount () {
+    this.getImages()
+  }
+
   render () {
     return (
       <div>
         <h1 className="text-center">Latest Uploads</h1>
-        <div className="col-sm-12">
+        <div className="col-sm-12 m-4">
+          <Images images={this.state.images}/>
         </div>
       </div>
     )
